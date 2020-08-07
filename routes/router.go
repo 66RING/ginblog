9f6d3e45fef24b969e613777666902a83090ceb8
66RING <1552087953@qq.com>
2020-08-07 21:11:21 +0800

    user add ang user list

--- routes/router.go
+++ routes/router.go
@@ -1,8 +1,8 @@
 package routes
 
 import (
+	v1 "ginblog/api/v1"
 	"ginblog/utils"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,11 +13,13 @@ func InitRouter() {
 
 	router := r.Group("api/v1")
 	{
-		router.GET("hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "ok",
-			})
-		})
+		// 用户 模块路由接口
+		router.POST("user/add", v1.AddUser)
+		router.GET("users", v1.GetUsers)
+		router.PUT("user/:id", v1.EditUser)
+		router.DELETE("user/:id", v1.DeleteUser)
+		// 分类 模块路由接口
+		// 文章 模块路由接口
 	}
 
 	r.Run(utils.HttpPort)
---
+++ utils/errmsg/errmsg.go
@@ -0,0 +1,33 @@
+package errmsg
+
+const (
+	SUCCSE = 200
+	ERROR  = 500
+
+	// code = 1000xxx 用户模块错误
+	ERROR_USERNAME_USED    = 1001
+	ERROR_PASSWORD_WRONG   = 1002
+	ERROR_USER_NOT_EXIST   = 1003
+	ERROR_TOKEN_EXIST      = 1004
+	ERROR_TOKEN_RUNTIME    = 1005
+	ERROR_TOKEN_WRONT      = 1006
+	ERROR_TOKEN_TYPE_WRONG = 1007
+	// code = 2000xxx 文章模块错误
+	// code = 3000xxx 分类模块错误
+)
+
+var codeMsg = map[int]string{
+	SUCCSE:                 "OK",
+	ERROR:                  "FAIL",
+	ERROR_USERNAME_USED:    "用户名已经存在",
+	ERROR_PASSWORD_WRONG:   "密码错误",
+	ERROR_USER_NOT_EXIST:   "用户不存在",
+	ERROR_TOKEN_EXIST:      "TOKEN不存在",
+	ERROR_TOKEN_RUNTIME:    "TOKEN已过期",
+	ERROR_TOKEN_WRONT:      "TOKEN不正确",
+	ERROR_TOKEN_TYPE_WRONG: "TOKEN格式错误",
+}
+
+func GetErrMsg(code int) string {
+	return codeMsg[code]
+}
